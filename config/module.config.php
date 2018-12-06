683f70d3f0326719df21a1e924c1d52a55eb290a
JiangningLIN <33124632+JiangningLIN@users.noreply.github.com>
2018-12-06 17:16:25 +0100

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -6,6 +6,12 @@ return [
         ],
         'aliases' => [
             'text/plain' => 'text_embed',
+			'text/html' => 'text_embed',
+			'text/x-php' => 'text_embed',
+			'text/css' => 'text_embed',
+			'text/xml' => 'text_embed',
+			'text/x-c++' => 'text_embed',
+			'text/csv' => 'text_embed',
             'text' => 'text_embed',
         ],
     ],
